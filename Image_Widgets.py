dc9a9a2a342536db1fce9900d0cdfc4e59ff24ca
Jeffery-Frazier-Jr <jeffery.frazier.jr@gmail.com>
2024-09-11 17:16:34 -0400

    File upload (contains some basic functionality)

---
+++ Image_Widgets.py
@@ -0,0 +1,21 @@
+import customtkinter as ctk
+from tkinter import filedialog, Canvas
+from settings import *
+
+class ImageImport(ctk.CTkFrame):
+    def __init__(self, parent, import_func):
+        super().__init__(parent)
+        self.grid(column = 0, columnspan = 2, row = 0, sticky = 'news')
+        self.import_func = import_func
+
+        ctk.CTkButton(self, text = 'open image', command = self.open_dialog).pack(expand = True)
+
+    def open_dialog(self):
+        path = filedialog.askopenfile().name
+        self.import_func(path)
+
+class ImageOutput(Canvas):
+    def __init__(self, parent, resize_image):
+        super().__init__(parent, bg = BACKGROUND_COLOR, bd = 0, highlightthickness = 0, relief = 'ridge')
+        self.grid(row = 0, column = 1, sticky = 'news')
+        self.bind('<Configure>', resize_image)
---
+++ settings.py
@@ -0,0 +1,20 @@
+# default vlaues
+ROTATE_DEFAULT = 0
+ZOOM_DEFAULT = 0
+FLIP_OPTIONS = ['None', 'X', 'Y', 'Both']
+BLUR_DEFAULT = 0
+CONTRAST_DEFAULT = 0
+EFFECT_OPTIONS = ['None', 'Emboss', 'Find edges', 'Contour', 'Edge enhance']
+BRIGHTNESS_DEFAULT = 1
+VIBRANCE_DEFAULT = 1
+GRAYSCALE_DEFAULT = False
+INVERT_DEFAULT = False
+
+# colors
+BACKGROUND_COLOR = '#242424'
+WHITE = '#FFF'
+GRAY = 'gray'
+BLUE = '#1F6AA5'
+DARK_GRAY = '4A4A4A'
+CLOSE_RED = '8A0606'
+SLIDER_BG ='64686B'
