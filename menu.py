d688e45b806d6940dcf635a7e3ca070be119db97
Jeffery-Frazier-Jr <jeffery.frazier.jr@gmail.com>
2024-09-12 16:50:43 -0400

    Rotation slider functionality

--- menu.py
+++ menu.py
@@ -2,7 +2,7 @@ import customtkinter as ctk
 from panels import *
 
 class Menu(ctk.CTkTabview):
-    def __init__(self, parent):
+    def __init__(self, parent, rotation):
         super().__init__(parent)
         self.grid(row = 0, column = 0, sticky = 'news', pady = 10, padx = 10)
 
@@ -13,15 +13,15 @@ class Menu(ctk.CTkTabview):
         self.add('Export')
 
         # widgets
-        PositionFrame(self.tab('Position'))
+        PositionFrame(self.tab('Position'), rotation)
 
 class PositionFrame(ctk.CTkFrame):
-    def __init__(self, parent):
+    def __init__(self, parent, rotation):
         super().__init__(parent, fg_color = 'transparent')
         self.pack(expand = True, fill = 'both')
 
-        SliderPanel(self, 'Rotation')
-        SliderPanel(self, 'Zoom')
+        SliderPanel(self, 'Rotation', rotation, 0, 360)
+        # SliderPanel(self, 'Zoom')
 
 class ColorFrame(ctk.CTkFrame):
     def __init__(self, parent):
