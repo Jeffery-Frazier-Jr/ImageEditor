da4625d3c427040a71620378df2d43b075c4467a
Jeffery-Frazier-Jr <jeffery.frazier.jr@gmail.com>
2024-09-12 22:15:32 -0400

    Image Editor complete fully functional

--- panels.py
+++ panels.py
@@ -1,4 +1,5 @@
 import customtkinter as ctk
+from tkinter import filedialog
 from settings import *
 
 class Panel(ctk.CTkFrame):
@@ -46,6 +47,47 @@ class SwitchPanel(Panel):
             switch = ctk.CTkSwitch(self, text = text, variable = var, button_color = BLUE, fg_color = SLIDER_BG)
             switch.pack(side = 'left', expand = True, fill = 'both', padx = 5, pady = 5)
 
+class FileNamePanel(Panel):
+    def __init__(self, parent, name_string, file_string):
+        super().__init__(parent = parent)
+
+        # data
+        self.name_string = name_string
+        self.name_string.trace('w', self.update_text)
+        self.file_string = file_string
+        
+        # check boxes for file format
+        ctk.CTkEntry(self, textvariable = self.name_string).pack(fill = 'x', padx = 20, pady = 5)
+        frame = ctk.CTkFrame(self, fg_color = 'transparent')
+        jpg_check = ctk.CTkCheckBox(frame, text = 'jpg', variable = self.file_string, command = lambda: self.click('jpg'), onvalue = 'jpg', offvalue = 'png')
+        jpg_check.pack(side = 'left', fill = 'x', expand = True)
+        png_check = ctk.CTkCheckBox(frame, text = 'png', variable = self.file_string, command = lambda: self.click('png'), onvalue = 'png', offvalue = 'jpg')
+        png_check.pack(side = 'left', fill = 'x', expand = True)
+        frame.pack(expand = True, fill = 'x', padx = 20)
+
+        # preview text
+        self.output = ctk.CTkLabel(self, text = '')
+        self.output.pack()
+
+    def click(self, value):
+        self.file_string.set(value)
+        self.update_text()
+
+    def update_text(self, *args):
+        if self.name_string.get():
+            text = self.name_string.get().replace(' ','_') + '.' + self.file_string.get()
+            self.output.configure(text = text)
+
+class FilePathPanel(Panel):
+    def __init__(self, parent, path_string):
+        super().__init__(parent = parent)
+        self.path_string = path_string
+
+        ctk.CTkButton(self, text = 'Open Explorer', command = self.open_file_dialog).pack(pady = 5)
+        ctk.CTkEntry(self, textvariable = self.path_string).pack(expand = True, fill = 'both', padx = 5, pady = 5)
+    def open_file_dialog(self):
+        self.path_string.set(filedialog.askdirectory())
+
 class DropDownPanel(ctk.CTkOptionMenu):
     def __init__(self, parent, data_var, options):
         super().__init__(
@@ -67,3 +109,19 @@ class RevertButton(ctk.CTkButton):
     def revert(self):
         for var, value in self.args:
             var.set(value)
+
+class SaveButton(ctk.CTkButton):
+    def __init__(self, parent, export_image, name_string, file_string, path_string):
+        super().__init__(parent, text = 'save', command = self.save)
+        self.pack(side = 'bottom', pady = 10)
+
+        self.export_image = export_image
+        self.name_string = name_string
+        self.file_string = file_string
+        self.path_string = path_string
+
+    def save(self):
+        self.export_image(
+            self.name_string.get(),
+            self.file_string.get(),
+            self.path_string.get())
